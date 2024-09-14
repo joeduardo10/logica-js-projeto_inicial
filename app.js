4a76bc364ac8ec16b984e423abfb7a0af678cd7f
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2024-09-14 11:21:06 -0300

    primeira aula

---
+++ app.js
@@ -0,0 +1,10 @@
+alert('Bem vindos ao jogo do número secreto');
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute = prompt('digite um numero entre 0 e 10');
+if(chute == numeroSecreto){
+    alert(`Isso aí vo acretou o número ${numeroSecreto}`)}
+else{
+    alert('Você errou!')};
+
+ 
