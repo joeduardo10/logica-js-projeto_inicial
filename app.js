9b8a52589926a259d829559ccbbde3643fe13f95
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2024-09-15 22:00:30 -0300

    mudei

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 alert('Bem vindos ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 4);
 console.log(numeroSecreto);
-let chute = prompt('digite um numero entre 0 e 10');
-if(chute == numeroSecreto){
-    alert(`Isso aí vo acretou o número ${numeroSecreto}`)}
-else{
-    alert('Você errou!')};
+let chute ;
+let tentativas = 1;
+while(chute != numeroSecreto){
+    chute = prompt('digite um numero entre 0 e 100');
+    if(chute == numeroSecreto){
+        break;
+    }else{
+            if(chute > numeroSecreto){
+                alert(`Ònumero secreto é menor que ${chute}`)}
+            else{
+                alert(`Ònumero secreto é maior que ${chute}`)}
+            tentativas ++;    
 
- 
+                }
+
+        }
+     
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+    
